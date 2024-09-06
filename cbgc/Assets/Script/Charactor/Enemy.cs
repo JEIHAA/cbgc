0ca6a6da43d2c869d4f9f4bfe3404df5f16fe1b4
JJH <61932935+junhoGithub12@users.noreply.github.com>
2024-09-06 16:03:04 +0900

    Compass Seperate, code fix

--- cbgc/Assets/Script/Charactor/Enemy.cs
+++ cbgc/Assets/Script/Charactor/Enemy.cs
@@ -8,44 +8,45 @@ public class Enemy : MonoBehaviour, IDamagable
     SpriteRenderer sr;
     Rigidbody2D rigid;
     Animator ani;
-    WaitForSeconds checkTime, knockBackTime;
+    WaitForSeconds knockBackTime;
     public bool isUpdate;
-    private Vector3 freePos;
-    private Vector3 addVelocity;
-    private Vector3 Velocity
+    private Vector2 addVelocity;
+    private Vector2 Velocity
     {
         get => rigid.velocity;
         set { rigid.velocity = value + addVelocity; sr.flipX = value.x < 0 ? true : false; }
     }
-    // Start is called before the first frame update
     void Start()
     {
+        //component
         rigid = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         ani = GetComponent<Animator>();
 
-        checkTime = new(1f);
         knockBackTime = new(0.125f);
 
         if (isUpdate) StartCoroutine(ControlableUpdate());
     }
+    //Update
     IEnumerator ControlableUpdate()
     {
+        //delay
         yield return new WaitForSeconds(0.125f);
         while (gameObject.activeSelf)
         {
+            //chase more bright light
             if (LightData.TorchIsBrightest) MoveToPlayer();
             else MoveToCenter();
             yield return null;
         }
     }
+    //add speed for knockback
     IEnumerator MoveBackToKnockBack()
     {
-        addVelocity = -rigid.velocity * 10;
+        addVelocity = -Velocity * 10;
         yield return knockBackTime;
         addVelocity = Vector3.zero;
     }
-    void MoveFreePosition() => Velocity = (freePos - transform.position).normalized * speed;
     void MoveToPlayer() => Velocity = (Player.playerTransform.position - transform.position).normalized * speed;
     void MoveToCenter() => Velocity = -transform.position.normalized * speed;
     public void OnDamage(float _damage) {
@@ -70,18 +71,20 @@ public class Enemy : MonoBehaviour, IDamagable
     {
         if (_collision.gameObject.tag == "Player")
             _collision.gameObject.GetComponent<Player>().OnDamage(100);
-        if(rigid != null) rigid.velocity = Vector2.zero;
     }
     private void OnTriggerEnter2D(Collider2D _collision)
     {
-        if (_collision.gameObject.tag == "Player" && !isUpdate)
+        switch (_collision.gameObject.tag)
         {
-            StartCoroutine(ControlableUpdate());
-        }
-        if (_collision.gameObject.tag == "PlayerAttack")
-        {
-            OnDamage(5);
-            if(gameObject.activeSelf) KnockBack();
+            case "Player":
+                if(!isUpdate) StartCoroutine(ControlableUpdate());
+                break;
+            case "PlayerAttack":
+                OnDamage(5);
+                if (gameObject.activeSelf) KnockBack();
+                break;
+            default:
+                break;
         }
     }
     public void KnockBack() => StartCoroutine(MoveBackToKnockBack());
