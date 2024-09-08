1d2de70f9886ee2f0a55ab827106af38525e2213
JJH <61932935+junhoGithub12@users.noreply.github.com>
2024-09-08 18:11:03 +0900

    Reapply "update object pool manager more short"
    This reverts commit 72754f656a2a10aa00683c35183a261fd54aa77d.

--- cbgc/Assets/Scripts/Manager/ObjectPoolManager.cs
+++ cbgc/Assets/Scripts/Manager/ObjectPoolManager.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
-
 public class ObjectPoolManager : MonoBehaviour
 {
     public static ObjectPoolManager instance;
@@ -22,40 +19,22 @@ public class ObjectPoolManager : MonoBehaviour
         EyeMonster,
         Tree,
     }
-    public ObjectPool<GameObject> GetPool(Pool pool)
-    {
-        return poolsInfo[(int)pool].objectPools;
-    }
+    public ObjectPool<GameObject> GetPool(Pool pool) => poolsInfo[(int)pool].objectPools;
     private void Awake()
     {
         instance = this;
-        //I dont know why using "for loop" makes index out error... :(
-        poolsInfo[0].objectPools = new ObjectPool<GameObject>(
-            () => Instantiate(poolsInfo[0].prefeb,transform),
+        for (int i = 0; i < poolsInfo.Length; i++) PoolInit(i);
+    }
+    void PoolInit(int idx)
+    {
+        poolsInfo[idx].objectPools = new ObjectPool<GameObject>(
+            () => Instantiate(poolsInfo[idx].prefeb, transform),
             (GameObject obj) => obj.SetActive(true),
             (GameObject obj) => obj.SetActive(false),
             (GameObject obj) => Destroy(obj),
             false,
-            poolsInfo[0].initialPoolSize,
-            poolsInfo[0].initialPoolSize
-            );
-        poolsInfo[1].objectPools = new ObjectPool<GameObject>(
-            () => Instantiate(poolsInfo[1].prefeb, transform),
-            (GameObject obj) => obj.SetActive(true),
-            (GameObject obj) => obj.SetActive(false),
-            (GameObject obj) => Destroy(obj),
-            false,
-            poolsInfo[1].initialPoolSize,
-            poolsInfo[1].initialPoolSize
-            );
-        poolsInfo[2].objectPools = new ObjectPool<GameObject>(
-            () => Instantiate(poolsInfo[2].prefeb, transform),
-            (GameObject obj) => obj.SetActive(true),
-            (GameObject obj) => obj.SetActive(false),
-            (GameObject obj) => Destroy(obj),
-            false,
-            poolsInfo[2].initialPoolSize,
-            poolsInfo[2].initialPoolSize
-            );
+            poolsInfo[idx].initialPoolSize,
+            poolsInfo[idx].initialPoolSize
+        );
     }
 }
