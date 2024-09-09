464e3abcae226d2faa88cbb0ad42f61a640f2a1c
JJH <61932935+junhoGithub12@users.noreply.github.com>
2024-09-09 10:05:40 +0900

    update player can attack behind

--- cbgc/Assets/Scripts/Charactor/PlayerAttack.cs
+++ cbgc/Assets/Scripts/Charactor/PlayerAttack.cs
@@ -6,22 +6,28 @@ public class PlayerAttack : MonoBehaviour
     public bool canAttack = true;
     [SerializeField]
     private float attackRange, attackDelay;
+    private float attackDir;
     private void Start()
     {
         gameObject.transform.localScale = Vector3.one * attackRange;
     }
     public void Attack()
     {
+        //block another attack while attack
         canAttack = false;
+        //attack dir
+        attackDir = (Player.playerTransform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x ? -1 : 1);
+        attackDir *= Player.playerTransform.localScale.x;
+        gameObject.transform.localScale = new Vector3(attackDir, 1, 1) * attackRange;
         //attack delay
         gameObject.SetActive(true);
         StartCoroutine(AttackObjectOff());
     }
     IEnumerator AttackObjectOff()
     {
-        
         yield return new WaitForSeconds(attackDelay);
-        canAttack = true;
+        //can attack
         gameObject.SetActive(false);
+        canAttack = true;
     }
 }
