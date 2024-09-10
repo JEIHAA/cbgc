a28acae2b19e9635aa5c3d33f22ba3d4b507400e
JJH <61932935+junhoGithub12@users.noreply.github.com>
2024-09-10 20:26:51 +0900

    Revert "fix player sprite flipx"
    This reverts commit ad1f13915ba1da0e433a26f762d5b960bd07123e.

--- cbgc/Assets/Scripts/Charactor/PlayerController.cs
+++ cbgc/Assets/Scripts/Charactor/PlayerController.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
 public class PlayerController : Controller
 {
-    [SerializeField]
-    SpriteRenderer sr;
     public override void Move()
     {
-        //Input
         Velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed + addVelocity;
-        //이팩트 방향 결정
         gameObject.transform.localScale = new Vector3((Player.playerTransform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x ? -1 : 1), 1, 1);
-        //스프라이트 방향 결정
-        if(Velocity.x != 0)
-        {
-            if (gameObject.transform.localScale.x < 0) sr.flipX = Velocity.x > 0;
-            else sr.flipX = Velocity.x < 0;
-        }
     }
 }
