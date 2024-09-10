c95cba041f00467e6cf913dd0cb80d6256a110ec
JJH <61932935+junhoGithub12@users.noreply.github.com>
2024-09-10 20:52:54 +0900

    update player dir

--- cbgc/Assets/Scripts/Charactor/PlayerController.cs
+++ cbgc/Assets/Scripts/Charactor/PlayerController.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 public class PlayerController : Controller
 {
+    public bool lookMouse;
     public override void Move()
     {
+        //이동 방향 입력받기
         Velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed + addVelocity;
-        gameObject.transform.localScale = new Vector3((Player.playerTransform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x ? -1 : 1), 1, 1);
+        //마우스를 바라봐야 한다면 마우스 보기 (자원 캐는동안, 공격하는 동안)
+        if (lookMouse)
+        {
+            gameObject.transform.localScale = new Vector3((Player.playerTransform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x ? -1 : 1), 1, 1);
+        }
+        //단순 이동
+        else if(Velocity.x != 0)
+        {
+            gameObject.transform.localScale = new Vector3(Velocity.x < 0 ? -1 : 1, 1, 1);
+        }
     }
 }
